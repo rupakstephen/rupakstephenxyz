834df10212bc76c652d243e1a2916bc46de1da75
rupakstephen <rupak.stephen@gmail.com>
2024-03-30 01:08:23 -0400

    restructuring app

---
+++ client/src/modules/App.js
@@ -0,0 +1,14 @@
+import '../css/App.css';
+import Navbar from  "./Navbar.js"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
