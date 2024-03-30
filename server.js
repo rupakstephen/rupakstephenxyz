f3b958a1e2cad0ade0d860575719e5a03ece6290
Rupak Stephen <rupak.stephen@gmail.com>
2024-03-30 00:32:55 -0400

    added in base code

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define routes and middleware
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const todoSchema = new mongoose.Schema({
+    task: String,
+    completed: Boolean,
+  });
+
+const Todo = mongoose.model('Todo', todoSchema);
