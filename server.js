29ce89ac9bafdb286174bb75fadd3582394d6f9f
Rupak Stephen <rupak.stephen@gmail.com>
2024-04-03 20:12:58 -0400

    commented out db stuff for now

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true, useUnifiedTopology: true });
+// // Connect to MongoDB
+// mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Define routes and middleware
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const todoSchema = new mongoose.Schema({
-    task: String,
-    completed: Boolean,
-  });
+// const todoSchema = new mongoose.Schema({
+//     task: String,
+//     completed: Boolean,
+//   });
 
-const Todo = mongoose.model('Todo', todoSchema);
+// const Todo = mongoose.model('Todo', todoSchema);
