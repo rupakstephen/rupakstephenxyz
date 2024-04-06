4ef7f9a58d666025692c418cfa71b4462ffa87d2
Rupak Stephen <rupak.stephen@gmail.com>
2024-04-06 11:43:34 -0400

    added in docker stuff

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
