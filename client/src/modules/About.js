71740c21d9d26318c6595319ff904730ec254e2d
Rupak Stephen <rupak.stephen@gmail.com>
2024-04-07 01:15:37 -0400

    started about page

--- client/src/modules/About.js
+++ client/src/modules/About.js
@@ -1,8 +1,61 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Stack from 'react-bootstrap/Stack';
+
+const leftJustify = {
+  "text-align": "left"
+}
+
 function About() { 
     return ( 
-       <div> 
-            <h1> This is the about page </h1> 
-      </div> 
+      <Container>
+      <Row>
+        <Col>
+          <br />
+          <h1> A bit about me...</h1>
+          <br />
+          <Stack style={leftJustify}>
+            <div>
+              <p>
+                I am a passionate and eager Software Engineer with a dual Computer Science and Information Technology degree. I am a
+                hard-working, sociable, teammate who believes in learning at every chance possible!
+              </p>
+            </div>
+            <div>
+              <h4>Work Experience</h4>
+              <Stack gap={2}>
+                <div>
+                  <h6>Assistant Staff, <i>MIT Lincoln Laboratory</i></h6>
+                  <p>
+                    Lorem ipsum dolor.
+                  </p>
+                </div>
+                <div>
+                  <h6>Corporate Business Intern, <i>Johnson and Johnson</i></h6>
+                  <p>
+                    Lorem ipsum dolor.
+                  </p>
+                </div>
+              </Stack>
+            </div>
+            <div>
+              <h4>Education</h4>
+              <Stack gap={2}>
+                <div>
+                  <h6>B.S., Computer Science/Information Technology, <i>Rensselaer Polytechnic Institute</i></h6>
+                  <ul>
+                    <li>GPA: 3.62</li>
+                    <li>Dean's List: Fall '19, Spring '20, Spring '21, Spring '22</li>
+                  </ul>
+                </div>
+              </Stack>
+            </div>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>  
     ); 
 } 
 export default About; 
