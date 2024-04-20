23fbe3b21cfeb603d79710b10454cb2c0f090566
Rupak Stephen <rupak.stephen@gmail.com>
2024-04-20 12:15:37 -0400

    pushing changes up for laptop work

--- server/server.js
+++ server/server.js
@@ -9,16 +9,32 @@ app.use(cors());
 app.use(express.json());
 
 // // Connect to MongoDB
-// mongoose.connect('mongodb://localhost/test-db', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://root:root@rupakstephenxyz.auj1iea.mongodb.net/");
 
 // Define routes and middleware
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// const todoSchema = new mongoose.Schema({
-//     task: String,
-//     completed: Boolean,
-//   });
+const ratingSchema = new mongoose.Schema({
+  media_name: String,
+  media_type: String,
+  rating: Number,
+  review: String,
+  tags: Array,
+  date: Date,
+  completed: Boolean,
+});
 
-// const Todo = mongoose.model('Todo', todoSchema);
+const ratingReview = mongoose.model("Rating", ratingSchema)
+
+// const testReview = new ratingReview({
+//   media_name:"Testing",
+//   media_type:"Test",
+//   rating:10,
+//   tags:["Teher","meher"],
+//   date:"10/31/2023",
+//   completed:true
+// })
+
+// testReview.save().then(() => console.log("Hello"))
