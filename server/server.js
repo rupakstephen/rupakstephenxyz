ba875a4cbdcbf4107721a7664b9931679ead1b4d
Rupak Stephen <rupak.stephen@gmail.com>
2024-04-21 20:17:24 -0400

    anonomyzed db URL

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASS = process.env.DB_PASS || "main";
+const DB_URL = process.env.DB_URL || "localhost";
 
 app.use(cors());
 app.use(express.json());
 
 // // Connect to MongoDB
-mongoose.connect("mongodb+srv://root:root@rupakstephenxyz.auj1iea.mongodb.net/?retryWrites=true&w=majority").then(() => {console.log("Connection to DB Established")})
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_URL}/?retryWrites=true&w=majority`).then(() => {console.log("Connection to DB Established")})
 
 // Define routes and middleware
 app.listen(PORT, () => {
@@ -28,17 +31,17 @@ const ratingSchema = new mongoose.Schema({
 
 const ratingReview = mongoose.model("Rating", ratingSchema)
 
-const testReview = new ratingReview({
-  media_name:"Testing",
-  media_type:"Test",
-  rating:10,
-  tags:["Teher","meher"],
-  date:"10/31/2023",
-  completed:true,
-  review_str:"Testing that this reivew actually works"
-})
+// const testReview = new ratingReview({
+//   media_name:"Testing",
+//   media_type:"Test",
+//   rating:10,
+//   tags:["Teher","meher"],
+//   date:"10/31/2023",
+//   completed:true,
+//   review_str:"Testing that this reivew actually works"
+// })
 
-testReview.save().then(() => console.log("Hello"))
+// testReview.save().then(() => console.log("Hello"))
 
 app.get("/reviews/all", async function (req, res) {
   let results = await ratingReview.find({})
